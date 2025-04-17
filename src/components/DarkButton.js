0230cb4566447d1a03ef3aec5accdd3389964a3e
Vanessa Bellotti <68295400+papillonlibre@users.noreply.github.com>
2025-04-17 14:39:53 -0400

    new button component with onClick and label props

---
+++ src/components/DarkButton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function DarkButton({label, onClick }) {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button 
+	  	onClick={onClick}
+        className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+        {label}
+      </button>
+    </div>
+  );
+}
+
+export default DarkButton;
--- src/components/SpotifyAuth.js
+++ src/components/SpotifyAuth.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import spotifyApi from '../utils/spotify';
 import Header from './Header'
+import DarkButton from './DarkButton';
 
 const SpotifyAuth = () => {
   const [token, setToken] = useState(localStorage.getItem('spotify_access_token'));
@@ -48,8 +49,7 @@ return (
       {token ? (
         <button onClick={handleLogout}>Logout</button>
       ) : (
-        // <button onClick={handleLogin}>Login to Spotify</button>
-        <button onClick={handleLogin} type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Login to Spotify</button>
+          <DarkButton onClick={handleLogin} label="Login with Spotify" />
       )}
       <h1 className="text-4xl font-bold text-blue-500 mt-10">
         Tailwind is Working! 🎉
